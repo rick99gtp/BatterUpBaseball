0d1c539c526e79d783d51d02eab12d7efb1c2335
rick99gtp <beyer.richard73@gmail.com>
2017-05-21 21:51:30 -0500

    Players_2016.db:
    Changed vs Left/Right ratings to a 1-10 range.
    Added defense_rating.
    
    Exhibition Select Pitcher:
    
    ~Removed overall rating column
    ~Added code to show 1st 2 layers of the player's card.
    ~Can touch player's card to show reverse.

--- app/src/main/java/com/batterupbaseball/exhibition_select_pitcher.java
+++ app/src/main/java/com/batterupbaseball/exhibition_select_pitcher.java
@@ -6,13 +6,17 @@ import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class exhibition_select_pitcher extends Activity implements View.OnClickListener {
+
     int[] llRow = {R.id.llRow1, R.id.llRow2, R.id.llRow3, R.id.llRow4, R.id.llRow5};
     int[] llThrows = {R.id.tvThrows1, R.id.tvThrows2, R.id.tvThrows3, R.id.tvThrows4, R.id.tvThrows5};
     int[] llName = {R.id.tvName1, R.id.tvName2, R.id.tvName3, R.id.tvName4, R.id.tvName5};
@@ -32,12 +36,14 @@ public class exhibition_select_pitcher extends Activity implements View.OnClickL
     int[] starterID = new int[5];
     String[] playerName = new String[5];
     String[] playerThrows = new String[5];
+    int[] playerDefense = new int[5];
     //int[] playerWins = new int[5];
     //int[] playerLosses = new int[5];
     //int[] playerPct = new int[5];
-    int[] playerVsLeft = new int[5];
-    int[] playerVsRight = new int[5];
+    int[] playerValueVsLeft = new int[5];
+    int[] playerValueVsRight = new int[5];
     int selectedStarter;
+    boolean cardViewVsRight = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +54,7 @@ public class exhibition_select_pitcher extends Activity implements View.OnClickL
         Bundle bundle=getIntent().getExtras();
         teamSelected = bundle.getString("USER_TEAM");
 
-        // prepare the Layouts
+        // prepare the Views
         for(int i=0; i<5; i++) {
             llStarter[i] = (LinearLayout) findViewById(llRow[i]);
             llStarter[i].setOnClickListener(this);
@@ -64,6 +70,15 @@ public class exhibition_select_pitcher extends Activity implements View.OnClickL
             public void onClick(View v) {
                 Intent intent = new Intent(exhibition_select_pitcher.this, exhibition_lineup.class);
                 startActivity(intent);
+            }
+        });
+
+        ImageView ivCard = (ImageView) findViewById(R.id.ivCard);
+        ivCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cardViewVsRight = !cardViewVsRight;
+                getCardImage();
             }
         });
 
@@ -102,6 +117,8 @@ public class exhibition_select_pitcher extends Activity implements View.OnClickL
         SharedPreferences.Editor editor = myPrefs.edit();
         editor.putInt("exhibition_selected_starter", selectedStarter);
         editor.apply();
+
+        getCardImage();
     }
 
     private void resetColors() {
@@ -111,15 +128,6 @@ public class exhibition_select_pitcher extends Activity implements View.OnClickL
     }
 
     private void getSeasonFileName() {
-        SharedPreferences myPrefs = getSharedPreferences("prefsFile", 0);
-        if(teamSelected.equals("V")) {
-            seasonID = myPrefs.getInt("vSeasonID", 1);
-            teamID = myPrefs.getInt("vTeamID", 1);
-        }
-        else {
-            seasonID = myPrefs.getInt("hSeasonID", 1);
-            teamID = myPrefs.getInt("hTeamID", 1);
-        }
 
         // open the season_owned database
         myDB = openOrCreateDatabase("seasons_owned.db", MODE_PRIVATE, null);
@@ -149,7 +157,6 @@ public class exhibition_select_pitcher extends Activity implements View.OnClickL
             for (int i = 0; i < 5; i++) {
                 int col = cTeam.getColumnIndex("sp" + (i + 1));
                 starterID[i] = cTeam.getInt(col);
-                Log.d("com.batterupbaseball", "" + starterID[i]);
             }
         }
 
@@ -174,12 +181,17 @@ public class exhibition_select_pitcher extends Activity implements View.OnClickL
                 playerThrows[i] = cPlayer.getString(col).toUpperCase();
 
                 col = cPlayer.getColumnIndex("vsl_rating");
-                playerVsLeft[i] = cPlayer.getInt(col);
+                playerValueVsLeft[i] = cPlayer.getInt(col);
 
                 col = cPlayer.getColumnIndex("vsr_rating");
-                playerVsRight[i] = cPlayer.getInt(col);
+                playerValueVsRight[i] = cPlayer.getInt(col);
+
+                col = cPlayer.getColumnIndex("defense_rating");
+                playerDefense[i] = cPlayer.getInt(col);
             }
         }
+
+        cPlayer.close();
 
         myDB.close();
     }
@@ -188,8 +200,8 @@ public class exhibition_select_pitcher extends Activity implements View.OnClickL
         for(int i=0; i<5; i++) {
             tvThrows[i].setText(playerThrows[i]);
             tvName[i].setText(playerName[i]);
-            tvVsLeft[i].setText("" + playerVsLeft[i]);
-            tvVsRight[i].setText("" + playerVsRight[i]);
+            tvVsLeft[i].setText("" + playerValueVsLeft[i]);
+            tvVsRight[i].setText("" + playerValueVsRight[i]);
         }
     }
 
@@ -203,9 +215,67 @@ public class exhibition_select_pitcher extends Activity implements View.OnClickL
         llStarter[selectedStarter].setBackgroundColor(Color.RED);
     }
 
+    private void getCardImage() {
+        int[] cardValue = {R.drawable.value_1, R.drawable.value_2, R.drawable.value_3, R.drawable.value_4, R.drawable.value_5, R.drawable.value_6, R.drawable.value_7, R.drawable.value_8, R.drawable.value_9, R.drawable.value_10};
+        ImageView ivCard = (ImageView) findViewById(R.id.ivCard);
+
+        Drawable[] layers = new Drawable[2];
+
+        switch(playerThrows[selectedStarter]) {
+            case "R":
+                if(cardViewVsRight) {
+                    // base
+                    layers[0] = ContextCompat.getDrawable(this, R.drawable.pitcher_card_vsr_as_right);
+                    // value
+                    layers[1] = ContextCompat.getDrawable(this, cardValue[playerValueVsRight[selectedStarter]-1]);
+                }
+                else {
+                    // base
+                    layers[0] = ContextCompat.getDrawable(this, R.drawable.pitcher_card_vsl_as_right);
+                    // value
+                    layers[1] = ContextCompat.getDrawable(this, cardValue[playerValueVsLeft[selectedStarter]-1]);
+                }
+
+                break;
+            case "L":
+                if(cardViewVsRight) {
+                    // base
+                    layers[0] = ContextCompat.getDrawable(this, R.drawable.pitcher_card_vsr_as_left);
+                    // value
+                    layers[1] = ContextCompat.getDrawable(this, cardValue[playerValueVsRight[selectedStarter]-1]);
+                }
+                else {
+                    // base
+                    layers[0] = ContextCompat.getDrawable(this, R.drawable.pitcher_card_vsl_as_left);
+                    // value
+                    layers[1] = ContextCompat.getDrawable(this, cardValue[playerValueVsLeft[selectedStarter]-1]);
+                }
+
+                break;
+            default:
+                break;
+        }
+
+        LayerDrawable layerDrawable = new LayerDrawable(layers);
+        ivCard.setImageDrawable(layerDrawable);
+
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
+
+        SharedPreferences myPrefs = getSharedPreferences("prefsFile", 0);
+
+        if(teamSelected.equals("V")) {
+            seasonID = myPrefs.getInt("vSeasonID", 1);
+            teamID = myPrefs.getInt("vTeamID", 1);
+        }
+        else {
+            seasonID = myPrefs.getInt("hSeasonID", 1);
+            teamID = myPrefs.getInt("hTeamID", 2);
+        }
+
         // get the season file name
         getSeasonFileName();
         // get rotation
@@ -214,5 +284,8 @@ public class exhibition_select_pitcher extends Activity implements View.OnClickL
 
         // highlight starter selected
         highlightStarter();
+
+        // get the card image and put in imageview
+        getCardImage();
     }
 }
